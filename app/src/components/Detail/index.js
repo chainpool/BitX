905a41ab374cf6f135a4fef8d7f757d6ee7c2502
Yongfeng LI <wliyongfeng@gmail.com>
2019-03-25 18:27:50 +0800

    Add Receive component

--- app/src/components/Detail/index.js
+++ app/src/components/Detail/index.js
@@ -8,8 +8,14 @@ import ArrowBack from '@material-ui/icons/ArrowBack';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import './index.scss';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Receive from './Receive';
 
 class Detail extends Component {
+  state = { value: 1 };
+
   componentDidMount() {
     const { getBalance } = this.props;
     getBalance();
@@ -20,6 +26,7 @@ class Detail extends Component {
     const { accounts, balances } = this.props;
     const balance = balances.find((balance) => balance.address === address) || {};
     const account = (accounts || []).find((account) => account.address === address) || {};
+    const { value } = this.state;
 
     return (
       <div className="detail">
@@ -40,6 +47,20 @@ class Detail extends Component {
           <h4 align="center">{balance.balance}BTC</h4>
           <p align="center">{balance.address}</p>
         </section>
+
+        <Paper square>
+          <Tabs
+            value={value}
+            variant="fullWidth"
+            indicatorColor="primary"
+            textColor="primary"
+            onChange={(event, newValue) => this.setState({ value: newValue })}>
+            <Tab label="发送" />
+            <Tab label="接收" />
+          </Tabs>
+
+          {value === 1 && <Receive address={address} />}
+        </Paper>
       </div>
     );
   }
