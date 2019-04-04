25f92a0e904e7063d313cbc953710a0ff89fa69c
Yongfeng LI <wliyongfeng@gmail.com>
2019-04-05 00:21:12 +0800

    Fix endpoint

--- app/src/service/index.js
+++ app/src/service/index.js
@@ -1,6 +1,6 @@
 const path = process.env.NODE_ENV === 'production' ? 'main' : 'test3';
 
-const endpoint = `https://api.blockcypher.com/v1/btc/${path}`;
+const endpoint = `https://api.blockcypher.com/v1/btc/test3`;
 
 export async function getBalance(addr) {
   return window.fetch(endpoint + `/addrs/${addr}/balance`).then((response) => {
