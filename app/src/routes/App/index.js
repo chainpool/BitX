120cff751304fe7b2002b32dabd20dd454baa319
weixiaoyi <2278095567@qq.com>
2019-04-08 14:32:13 +0800

    移动端样式

--- app/src/routes/App/index.js
+++ app/src/routes/App/index.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Switch } from 'react-router';
 import CommonLayOut from './CommonLayOut';
+import { Device } from '../../utils';
 
 import './index.module.scss';
 
 class App extends Component {
+  componentDidMount() {
+    if (Device.isMobile()) {
+      document.getElementById('root').setAttribute('class', 'root-mobile');
+    } else {
+      document.getElementById('root').setAttribute('class', 'root-desktop');
+    }
+  }
   render() {
     return (
       <Router>
