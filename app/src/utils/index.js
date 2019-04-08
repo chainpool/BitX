610a8e976978ed5e273c074234dc0af0a0107315
weixiaoyi <2278095567@qq.com>
2019-04-08 17:06:41 +0800

    fetch封装

--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -7,6 +7,7 @@ import { BigNumber } from 'bignumber.js';
 import { default as queryString } from 'query-string';
 import WAValidator from 'wallet-address-validator';
 import device from 'current-device';
+import uniqid from 'uniqid';
 
 export const isEmpty = (value) => {
   return isNaN(value) || value === undefined || value === '';
@@ -183,4 +184,54 @@ export const bitJS = {
 export const Device = {
   isMobile: () => device.mobile(),
   isDesktop: () => device.desktop(),
+};
+
+export const fetchFromHttp = ({
+  url,
+  method = 'POST',
+  methodAlias,
+  params = [],
+  body,
+  timeOut = 10000,
+}) => {
+  const id = uniqid();
+  const message = body
+    ? JSON.stringify(body)
+    : JSON.stringify({ id, jsonrpc: '2.0', method: methodAlias, params });
+  const request = () =>
+    fetch(url, {
+      method: method,
+      headers: {
+        method,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      ...(method.toUpperCase() === 'GET' ? {} : { body: message }),
+    })
+      .then(async (res) => {
+        if (res.status >= 200 && res.status < 300) {
+          return res.json();
+        } else {
+          const result = await res.json();
+          return Promise.reject({
+            status: res.status,
+            message: result,
+          });
+        }
+      })
+      .catch((err) => {
+        return Promise.reject(err);
+      });
+  if (timeOut) {
+    return Promise.race([
+      request(),
+      new Promise((resovle, reject) => {
+        setTimeout(() => {
+          reject('请求超时');
+        }, timeOut);
+      }),
+    ]);
+  } else {
+    return request();
+  }
 };
