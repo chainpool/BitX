669dd8b2dabae196df081ae277cecf361e392592
Yongfeng LI <wliyongfeng@gmail.com>
2019-04-19 12:25:26 +0800

    Add api to get utxos and balance

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,12 @@
 const fetch = require("node-fetch");
 const Router = require("koa-router");
+const pick = require("lodash.pick");
 
 const defaultEndpoint = "http://47.111.89.46:18332";
+const defaultBitcoreEndpoint = "http://47.111.89.46:3000";
 
 const endpoint = process.env.BTC_END_POINT || defaultEndpoint;
+const bitcoreEndpoint = process.env.BITCORE_END_POINT || defaultBitcoreEndpoint;
 
 const router = new Router();
 
@@ -12,7 +15,6 @@ router.get("/ping", async ctx => {
 });
 
 router.post("/txs", async ctx => {
-  console.log(ctx.params);
   const { raw } = ctx.request.body;
   if (!raw) {
     ctx.status = 400;
@@ -28,6 +30,30 @@ router.post("/txs", async ctx => {
     ctx.status = 400;
     ctx.body = result.error;
   }
+});
+
+router.get("/testnet/:address/utxos", async ctx => {
+  const { address } = ctx.params;
+
+  const response = await fetch(
+    `${bitcoreEndpoint}/api/BTC/testnet/address/${address}?unspent=true&limit=1000`
+  );
+  const rawUtxos = await response.json();
+
+  ctx.body = rawUtxos.map(utxo =>
+    pick(utxo, ["mintTxid", "mintIndex", "value", "script"])
+  );
+});
+
+router.get("/testnet/:address/balance", async ctx => {
+  const { address } = ctx.params;
+
+  const response = await fetch(
+    `${bitcoreEndpoint}/api/BTC/testnet/address/${address}/balance`
+  );
+
+  const balance = await response.json();
+  ctx.body = balance;
 });
 
 module.exports = app => {
