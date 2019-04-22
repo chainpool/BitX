95032ff4799c2fb1d5b1b239dc49315557f40ce3
weixiaoyi <2278095567@qq.com>
2019-04-22 10:53:00 +0800

    fastClick

--- app/src/index.js
+++ app/src/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
-import './index.scss';
-import App from './routes/App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './store';
-import 'whatwg-fetch';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import initReactFastclick from "react-fastclick";
+import "normalize.css";
+import "./index.scss";
+import App from "./routes/App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store";
+import "whatwg-fetch";
+import thunk from "redux-thunk";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+initReactFastclick();
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
