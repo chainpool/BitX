fef919341b2561cb5410dd36b38cbf4e30f24fd8
Yongfeng LI <wliyongfeng@gmail.com>
2019-04-29 15:26:00 +0800

    Make server support mainnet

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,7 +5,9 @@ const pick = require("lodash.pick");
 const defaultEndpoint = "http://47.111.89.46:18332";
 const defaultBitcoreEndpoint = "http://47.111.89.46:3000";
 
-const endpoint = process.env.BTC_END_POINT || defaultEndpoint;
+const btcTestnetEndpoint = process.env.BTC_TESTNET_END_POINT || defaultEndpoint;
+const btcMainnetEndpoint =
+  process.env.BTC_MAINNET_END_POINT || "http://47.111.89.46:8332";
 const bitcoreEndpoint = process.env.BITCORE_END_POINT || defaultBitcoreEndpoint;
 
 const router = new Router();
@@ -15,13 +17,13 @@ router.get("/ping", async ctx => {
 });
 
 router.post("/txs", async ctx => {
-  const { raw } = ctx.request.body;
+  const { raw, network } = ctx.request.body;
   if (!raw) {
     ctx.status = 400;
     ctx.body = { error: "empty tx" };
     return;
   }
-  const response = await submit(raw);
+  const response = await submit(raw, network);
   const result = await response.json();
   if (response.ok) {
     ctx.status = 201;
@@ -30,6 +32,19 @@ router.post("/txs", async ctx => {
     ctx.status = 400;
     ctx.body = result.error;
   }
+});
+
+router.get("/mainnet/:address/utxos", async ctx => {
+  const { address } = ctx.params;
+
+  const response = await fetch(
+    `${bitcoreEndpoint}/api/BTC/mainnet/address/${address}?unspent=true&limit=1000`
+  );
+  const rawUtxos = await response.json();
+
+  ctx.body = rawUtxos.map(utxo =>
+    pick(utxo, ["mintTxid", "mintIndex", "value", "script"])
+  );
 });
 
 router.get("/testnet/:address/utxos", async ctx => {
@@ -43,6 +58,17 @@ router.get("/testnet/:address/utxos", async ctx => {
   ctx.body = rawUtxos.map(utxo =>
     pick(utxo, ["mintTxid", "mintIndex", "value", "script"])
   );
+});
+
+router.get("/mainnet/:address/balance", async ctx => {
+  const { address } = ctx.params;
+
+  const response = await fetch(
+    `${bitcoreEndpoint}/api/BTC/mainnet/address/${address}/balance`
+  );
+
+  const balance = await response.json();
+  ctx.body = balance;
 });
 
 router.get("/testnet/:address/balance", async ctx => {
@@ -61,7 +87,7 @@ module.exports = app => {
 };
 
 let id = 0;
-async function submit(raw) {
+async function submit(raw, network) {
   const obj = {
     id: id++,
     jsonrpc: "2.0",
@@ -80,6 +106,9 @@ async function submit(raw) {
     method: "POST"
   };
 
-  const response = await fetch(endpoint, request);
+  const response = await fetch(
+    network === "mainnet" ? btcMainnetEndpoint : btcTestnetEndpoint,
+    request
+  );
   return response;
 }
