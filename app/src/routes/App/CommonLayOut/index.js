4573d5833bc7f22c587b6c9cadf8e2ccd191b170
Yongfeng LI <wliyongfeng@gmail.com>
2019-04-29 18:10:53 +0800

    Fix import

--- app/src/routes/App/CommonLayOut/index.js
+++ app/src/routes/App/CommonLayOut/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, Redirect } from "react-router";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import routers from "../routers";
 import Header from "./Header";
