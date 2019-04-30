14b12fc4cb37baa9c3cb26d761de911a15bb8e89
Yongfeng LI <wliyongfeng@gmail.com>
2019-04-30 10:34:51 +0800

    Remove the useless balance state

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import accounts from "./reducers/account";
 import newMnemonic from "./reducers/mnemonic";
-import balances from "./reducers/balance";
 import utxos from "./reducers/utxo";
 import pageTitle from "./reducers/pageTitle";
 import modal from "./reducers/modal";
@@ -10,7 +9,6 @@ import goBack from "./reducers/goBack";
 export default combineReducers({
   accounts,
   newMnemonic,
-  balances,
   utxos,
   pageTitle,
   modal,
--- app/src/store/reducers/balance.js
+++
@@ -1,8 +0,0 @@
-const balances = (state = [], action) => {
-  if (action.type === 'ADD_BALANCE') {
-    return [action.balance, ...state];
-  }
-  return state;
-};
-
-export default balances;
