edc2320fa94c8e0596da6bbd55a8b4a8ef2dfa8a
Yongfeng LI <wliyongfeng@gmail.com>
2019-04-30 16:43:30 +0800

    fetch mainnet data with blockcypher api

--- app/src/service/index.js
+++ app/src/service/index.js
@@ -1,19 +1,46 @@
 import { fetchFromHttp } from "../utils";
 
 const testnetEndpoint = "https://api.chainx.org/bitx/testnet";
-const mainnetEndpoint = "https://api.chainx.org/bitx/mainnet";
 const submitEndpoint = "https://api.chainx.org/bitx/txs";
 
 export async function getBalance(addr, network = "testnet") {
-  const endpoint = network === "mainnet" ? mainnetEndpoint : testnetEndpoint;
-  return window.fetch(endpoint + `/${addr}/balance`).then(response => {
+  if (network === "mainnet") {
+    // TODO: 主网数据正在同步，暂时用blockcypher的API
+    return window
+      .fetch(`https://api.blockcypher.com/v1/btc/main/addrs/${addr}/balance`)
+      .then(response => response.json())
+      .then(data => {
+        return {
+          balance: data.final_balance,
+          confirmed: data.balance,
+          unconfirmed: data.unconfirmed_balance
+        };
+      });
+  }
+  return window.fetch(testnetEndpoint + `/${addr}/balance`).then(response => {
     return response.json();
   });
 }
 
 export async function getUtxos(addr, network = "testnet") {
-  const endpoint = network === "mainnet" ? mainnetEndpoint : testnetEndpoint;
-  return window.fetch(endpoint + `/${addr}/utxos`).then(res => res.json());
+  if (network === "mainnet") {
+    // TODO: 主网数据正在同步，暂时用blockcypher的API
+    return window
+      .fetch(
+        `https://api.blockcypher.com/v1/btc/main/addrs/${addr}?unspentOnly=true&confirmations=1`
+      )
+      .then(response => response.json())
+      .then(data => {
+        return (data.txrefs || []).map(tx => ({
+          mintTxid: tx.tx_hash,
+          mintIndex: tx.tx_output_n,
+          value: tx.value
+        }));
+      });
+  }
+  return window
+    .fetch(testnetEndpoint + `/${addr}/utxos`)
+    .then(res => res.json());
 }
 
 export async function broadcastTx(tx, network = "testnet") {
