8d629579f64dabebfc61bc62a30a3371c9a37cfa
Yongfeng LI <wliyongfeng@gmail.com>
2019-06-25 14:32:20 +0800

    Get balance and utxos with chain.so api

--- app/src/service/index.js
+++ app/src/service/index.js
@@ -5,36 +5,32 @@ const submitEndpoint = "https://api.chainx.org/bitx/txs";
 
 export async function getBalance(addr, network = "testnet") {
   // TODO: 主网数据正在同步，暂时用blockcypher的API
+  const endpoint = "https://chain.so/api/v2/get_address_balance/";
   return window
-    .fetch(
-      `https://api.blockcypher.com/v1/btc/${
-        network === "mainnet" ? "main" : "test3"
-      }/addrs/${addr}/balance`
-    )
+    .fetch(`${endpoint}${network === "mainnet" ? "btc" : "btctest"}/${addr}`)
     .then(response => response.json())
-    .then(data => {
+    .then(({ data }) => {
+      const confirmed = data.confirmed_balance * Math.pow(10, 8);
+      const unConfirmed = data.unconfirmed_balance * Math.pow(10, 8);
       return {
-        balance: data.final_balance,
-        confirmed: data.balance,
-        unconfirmed: data.unconfirmed_balance
+        balance: confirmed,
+        confirmed: confirmed,
+        unconfirmed: unConfirmed
       };
     });
 }
 
 export async function getUtxos(addr, network = "testnet") {
   // TODO: 主网数据正在同步，暂时用blockcypher的API
+  const endpoint = "https://chain.so/api/v2/get_tx_unspent/";
   return window
-    .fetch(
-      `https://api.blockcypher.com/v1/btc/${
-        network === "mainnet" ? "main" : "test3"
-      }/addrs/${addr}?unspentOnly=true&confirmations=1`
-    )
+    .fetch(`${endpoint}${network === "mainnet" ? "btc" : "btctest"}/${addr}`)
     .then(response => response.json())
-    .then(data => {
-      return (data.txrefs || []).map(tx => ({
-        mintTxid: tx.tx_hash,
-        mintIndex: tx.tx_output_n,
-        value: tx.value
+    .then(({ data }) => {
+      return (data.txs || []).map(tx => ({
+        mintTxid: tx.txid,
+        mintIndex: tx.output_no,
+        value: tx.value * Math.pow(10, 8)
       }));
     });
 }
