0e2d4790b96187076fcb68a91f831282db3a6adb
Yongfeng LI <wliyongfeng@gmail.com>
2019-07-09 17:38:54 +0800

    Update react-redux and refactor Modal component

--- app/src/components/Modal/index.js
+++ app/src/components/Modal/index.js
@@ -1,64 +1,44 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import * as styles from "./index.module.scss";
 import { setModal } from "../../store/actions";
 import { Device } from "../../utils";
 import classNames from "classnames";
 
-class Modal extends Component {
-  render() {
-    const {
-      title,
-      setModal,
-      modal: { show } = {},
-      children,
-      className,
-      closeCb
-    } = this.props;
-    return (
-      show && (
-        <div
-          className={classNames(
-            styles.Modalcontainer,
-            Device.isMobile() ? styles.rootModalMobile : styles.rootModalDesktop
+function Modal(props) {
+  const { title, children, className, closeCb } = props;
+
+  const { show } = useSelector(state => state.modal) || {};
+  const dispatch = useDispatch();
+
+  return (
+    show && (
+      <div
+        className={classNames(
+          styles.Modalcontainer,
+          Device.isMobile() ? styles.rootModalMobile : styles.rootModalDesktop
+        )}
+        id="rootModal"
+      >
+        <div className={classNames(styles.content, className)}>
+          {title && (
+            <div
+              className={styles.header}
+              onClick={() => {
+                closeCb && closeCb();
+                dispatch(setModal({ show: false }));
+              }}
+            >
+              <div className={styles.title}>{title}</div>
+              <i className="iconfont iconClose" />
+            </div>
           )}
-          id="rootModal"
-        >
-          <div className={classNames(styles.content, className)}>
-            {title && (
-              <div
-                className={styles.header}
-                onClick={() => {
-                  closeCb && closeCb();
-                  setModal(false);
-                }}
-              >
-                <div className={styles.title}>{title}</div>
-                <i className="iconfont iconClose" />
-              </div>
-            )}
-            {children}
-          </div>
+          {children}
         </div>
-      )
-    );
-  }
+      </div>
+    )
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    modal: state.modal
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    setModal: status => dispatch(setModal({ show: status }))
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Modal);
+export default Modal;
