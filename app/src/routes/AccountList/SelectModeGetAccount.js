76eed54c9ca87432c2c4b885a6efd30b17028f97
Yongfeng LI <wliyongfeng@gmail.com>
2019-07-09 18:52:31 +0800

    Refactor SelectModeGetAccount with react hooks

--- app/src/routes/AccountList/SelectModeGetAccount.js
+++ app/src/routes/AccountList/SelectModeGetAccount.js
@@ -1,105 +1,91 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { RouterGo } from "../../components";
 import { PATH } from "../../constants";
 import BitX from "../../resources/Bitx.png";
 import * as styles from "./SelectModeGetAccount.module.scss";
 import { setNetwork } from "../../store/actions";
 
-class SelectModeGetAccount extends Component {
-  state = {
-    isCreate: true,
-    openNetworkSelection: false
-  };
+function SelectModeGetAccount(props) {
+  const [isCreate, setIsCreate] = useState(true);
+  const [openNetworkSelection, setOpenNetworkSelection] = useState(false);
+  const dispatch = useDispatch();
 
-  render() {
-    const { changeClose, accounts = [], setNetwork } = this.props;
+  const { changeClose, accounts = [] } = props;
 
-    const { isCreate, openNetworkSelection } = this.state;
-
-    return (
-      <div className={styles.SelectModeGetAccount}>
-        {accounts.length ? (
-          <div className={styles.close} onClick={changeClose}>
-            <i className="iconfont iconClose" style={{ cursor: "pointer" }} />
-          </div>
-        ) : null}
-
-        <div className={styles.bitx}>
-          <img alt="" src={BitX} height={55} />
+  return (
+    <div className={styles.SelectModeGetAccount}>
+      {accounts.length ? (
+        <div className={styles.close} onClick={changeClose}>
+          <i className="iconfont iconClose" style={{ cursor: "pointer" }} />
         </div>
-        <div className={styles.routerbutton}>
-          <div>
-            <button
-              onClick={() =>
-                this.setState({ isCreate: true, openNetworkSelection: true })
-              }
-            >
-              创建账户
-            </button>
-          </div>
-          <div>
-            <button
-              onClick={() =>
-                this.setState({ isCreate: false, openNetworkSelection: true })
-              }
-            >
-              导入账户
-            </button>
-          </div>
-        </div>
+      ) : null}
 
-        {openNetworkSelection ? (
-          <section
-            className={styles.networkSelection}
-            onClick={() => this.setState({ openNetworkSelection: false })}
-          >
-            <main onClick={event => event.stopPropagation()}>
-              <header>
-                <h3>选择网络</h3>
-                <span
-                  onClick={() => this.setState({ openNetworkSelection: false })}
-                >
-                  <i
-                    className="iconfont iconClose"
-                    style={{ cursor: "pointer" }}
-                  />
-                </span>
-              </header>
-              <div>
-                <RouterGo
-                  onClick={() => setNetwork("mainnet")}
-                  go={{
-                    pathname: isCreate ? PATH.createaccount : PATH.importaccount
-                  }}
-                >
-                  主网
-                </RouterGo>
-                <div className={styles.separator} />
-                <RouterGo
-                  onClick={() => setNetwork("testnet")}
-                  go={{
-                    pathname: isCreate ? PATH.createaccount : PATH.importaccount
-                  }}
-                >
-                  测试网
-                </RouterGo>
-              </div>
-            </main>
-          </section>
-        ) : null}
+      <div className={styles.bitx}>
+        <img alt="" src={BitX} height={55} />
       </div>
-    );
-  }
+      <div className={styles.routerbutton}>
+        <div>
+          <button
+            onClick={() => {
+              setIsCreate(true);
+              setOpenNetworkSelection(true);
+            }}
+          >
+            创建账户
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setIsCreate(false);
+              setOpenNetworkSelection(true);
+            }}
+          >
+            导入账户
+          </button>
+        </div>
+      </div>
+
+      {openNetworkSelection ? (
+        <section
+          className={styles.networkSelection}
+          onClick={() => setOpenNetworkSelection(false)}
+        >
+          <main onClick={event => event.stopPropagation()}>
+            <header>
+              <h3>选择网络</h3>
+              <span onClick={() => setOpenNetworkSelection(false)}>
+                <i
+                  className="iconfont iconClose"
+                  style={{ cursor: "pointer" }}
+                />
+              </span>
+            </header>
+            <div>
+              <RouterGo
+                onClick={() => dispatch(setNetwork("mainnet"))}
+                go={{
+                  pathname: isCreate ? PATH.createaccount : PATH.importaccount
+                }}
+              >
+                主网
+              </RouterGo>
+              <div className={styles.separator} />
+              <RouterGo
+                onClick={() => dispatch(setNetwork("testnet"))}
+                go={{
+                  pathname: isCreate ? PATH.createaccount : PATH.importaccount
+                }}
+              >
+                测试网
+              </RouterGo>
+            </div>
+          </main>
+        </section>
+      ) : null}
+    </div>
+  );
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    setNetwork: network => dispatch(setNetwork(network))
-  };
-};
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(SelectModeGetAccount);
+export default SelectModeGetAccount;
