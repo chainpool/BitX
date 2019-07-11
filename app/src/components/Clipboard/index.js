2e96f7605b201e1722ee85ce9de5981e2ead8805
Yongfeng LI <wliyongfeng@gmail.com>
2019-07-11 18:23:48 +0800

    Refactor Clipboard with react hooks

--- app/src/components/Clipboard/index.js
+++ app/src/components/Clipboard/index.js
@@ -1,45 +1,38 @@
-import React, { PureComponent } from "react";
+import React, { useEffect } from "react";
 import ClipboardJS from "clipboard";
 import classNames from "classnames";
 import { Tooltip } from "../index";
 import uniqid from "uniqid";
 import * as styles from "./index.module.scss";
 
-export default class Clipboard extends PureComponent {
-  state = {
-    uid: this.props.id || uniqid("clipboard_")
-  };
+export default function Clipboard(props) {
+  useEffect(() => {
+    const clipBoard = new ClipboardJS(".clipboard");
 
-  componentDidMount() {
-    const { outInner = "" } = this.props;
-    new ClipboardJS(outInner ? ".outerInner" : ".clipboard");
-  }
+    return function() {
+      clipBoard.destroy();
+    };
+  }, []);
 
-  render() {
-    const { uid } = this.state;
-    const { children, className, width, outInner = "" } = this.props;
-    return (
-      <span className={classNames(styles.clipboard, className)}>
-        <span
-          id={uid}
-          className={classNames(styles.children, width ? styles.ellipse : null)}
-          style={{ width }}
-        >
-          {children}
-        </span>
-        <Tooltip tip="复制成功" type="click">
-          <i
-            className="clipboard iconfont iconfuzhi"
-            data-clipboard-target={`#${uid}`}
-            style={{ marginLeft: children ? 8 : null, cursor: "pointer" }}
-          />
-          {outInner ? (
-            <span className="outerInner" data-clipboard-target={`#${uid}`}>
-              {outInner}
-            </span>
-          ) : null}
-        </Tooltip>
+  const uid = props.id || uniqid("clipboard_");
+
+  const { children, className, width } = props;
+  return (
+    <span className={classNames(styles.clipboard, className)}>
+      <span
+        id={uid}
+        className={classNames(styles.children, width ? styles.ellipse : null)}
+        style={{ width }}
+      >
+        {children}
       </span>
-    );
-  }
+      <Tooltip tip="复制成功" type="click">
+        <i
+          className="clipboard iconfont iconfuzhi"
+          data-clipboard-target={`#${uid}`}
+          style={{ marginLeft: children ? 8 : null, cursor: "pointer" }}
+        />
+      </Tooltip>
+    </span>
+  );
 }
