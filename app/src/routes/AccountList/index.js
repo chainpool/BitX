2fedb123d7f25ef2ef750ccf43407ac3a65e2033
Yongfeng LI <wliyongfeng@gmail.com>
2019-07-12 15:20:34 +0800

    Refactor AccountList with hooks

--- app/src/routes/AccountList/index.js
+++ app/src/routes/AccountList/index.js
@@ -1,75 +1,68 @@
-import React from "react";
-import { Mixin, RouterGo } from "../../components";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { RouterGo } from "../../components";
 import { setBlankSpace } from "../../utils";
 import SelectModeGetAccount from "./SelectModeGetAccount";
 import * as styles from "./index.module.scss";
 import { PATH } from "../../constants";
+import { getAllAccountBalance } from "../../store/actions";
 
-class AccountList extends Mixin {
-  pageTitle = "BitX";
-  constructor(props) {
-    super(props);
-    const { accounts = [] } = props;
-    this.state = {
-      close: !!accounts.length
-    };
-  }
+function AccountList(props) {
+  const [close, setClose] = useState((props.accounts || []).length > 0);
+  const dispatch = useDispatch();
 
-  startInit = () => {
-    this.props.getAllAccountBalance();
-  };
+  useEffect(() => {
+    props.setPageTitle("BitX");
+  }, []);
 
-  changeClose = () => {
-    this.setState(prevState => ({
-      close: !prevState.close
-    }));
-  };
+  useEffect(() => {
+    dispatch(getAllAccountBalance());
+  }, []);
 
-  render() {
-    const { changeClose } = this;
-    const { close } = this.state;
-    const { accounts = [] } = this.props;
-
-    return (
-      <div className={styles.AccountList}>
-        <div className={styles.listtitle}>
-          <div>账户列表</div>
-          <div onClick={changeClose}>
-            <i className="iconfont iconadd" style={{ cursor: "pointer" }} />
-          </div>
+  return (
+    <div className={styles.AccountList}>
+      <div className={styles.listtitle}>
+        <div>账户列表</div>
+        <div onClick={() => setClose(!close)}>
+          <i className="iconfont iconadd" style={{ cursor: "pointer" }} />
         </div>
-        <ul>
-          {accounts.map((item, index) => (
-            <RouterGo
-              key={index}
-              Ele="li"
-              go={{
-                pathname: PATH.accountdetail,
-                search: `?address=${item.address}`
-              }}
-            >
-              <div className={styles.seperate} />
-              <div className={styles.desc}>
-                <div className={styles.title}>
-                  <span className={styles.name}>{item.name}</span>
-                  {item.network !== "mainnet" ? (
-                    <span className={styles.badge}>测试网</span>
-                  ) : null}
-                </div>
-                <div className={styles.amount}>
-                  {setBlankSpace(item.balanceShow, "BTC")}
-                </div>
-              </div>
-              <div className={styles.address}>{item.address}</div>
-            </RouterGo>
-          ))}
-        </ul>
-        {close ? null : (
-          <SelectModeGetAccount {...this.props} changeClose={changeClose} />
-        )}
       </div>
-    );
-  }
+      <ul>
+        {(props.accounts || []).map((item, index) => (
+          <RouterGo
+            key={index}
+            Ele="li"
+            go={{
+              pathname: PATH.accountdetail,
+              search: `?address=${item.address}`
+            }}
+          >
+            <div className={styles.seperate} />
+            <div className={styles.desc}>
+              <div className={styles.title}>
+                <span className={styles.name}>{item.name}</span>
+                {item.network !== "mainnet" ? (
+                  <span className={styles.badge}>测试网</span>
+                ) : null}
+              </div>
+              <div className={styles.amount}>
+                {setBlankSpace(item.balanceShow, "BTC")}
+              </div>
+            </div>
+            <div className={styles.address}>{item.address}</div>
+          </RouterGo>
+        ))}
+      </ul>
+      {close ? null : (
+        <SelectModeGetAccount
+          {...props}
+          changeClose={() => {
+            setClose(!close);
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default AccountList;
