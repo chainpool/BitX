41b3c2948ae329f489efbfc9f2fbfd505d1680d6
Yongfeng LI <wliyongfeng@gmail.com>
2019-07-12 15:30:24 +0800

    Refactor AccountInfo

--- app/src/routes/AccountDetail/AccountInfo.js
+++ app/src/routes/AccountDetail/AccountInfo.js
@@ -1,34 +1,32 @@
 import React from "react";
-import { Clipboard, Mixin, RouterGo } from "../../components";
+import { Clipboard, RouterGo } from "../../components";
 import * as styles from "./AccountInfo.module.scss";
 
-export default class AccountInfo extends Mixin {
-  render() {
-    const { currentAccount = {} } = this.props;
-    const isTestnet = currentAccount.network !== "mainnet";
+export default function AccountInfo(props) {
+  const { currentAccount = {} } = props;
+  const isTestNet = currentAccount.network !== "mainnet";
 
-    return (
-      <div className={styles.AccountInfo}>
-        <div className={styles.amount}>
-          {currentAccount.balanceShow}
-          <span>BTC</span>
-        </div>
-        <div className={styles.address}>
-          <Clipboard>{currentAccount.address}</Clipboard>
-        </div>
-        <div className={styles.watchother}>
-          <RouterGo
-            isOutSide
-            go={{
-              pathname: `https://live.blockcypher.com/btc${
-                isTestnet ? "-testnet" : ""
-              }/address/${currentAccount.address}/`
-            }}
-          >
-            在浏览器中查看
-          </RouterGo>
-        </div>
+  return (
+    <div className={styles.AccountInfo}>
+      <div className={styles.amount}>
+        {currentAccount.balanceShow}
+        <span>BTC</span>
       </div>
-    );
-  }
+      <div className={styles.address}>
+        <Clipboard>{currentAccount.address}</Clipboard>
+      </div>
+      <div className={styles.watchother}>
+        <RouterGo
+          isOutSide
+          go={{
+            pathname: `https://live.blockcypher.com/btc${
+              isTestNet ? "-testnet" : ""
+            }/address/${currentAccount.address}/`
+          }}
+        >
+          在浏览器中查看
+        </RouterGo>
+      </div>
+    </div>
+  );
 }
