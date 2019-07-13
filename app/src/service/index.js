2a879d50b01a97c639fd999adb8d4629bd2d6fc5
Yongfeng LI <wliyongfeng@gmail.com>
2019-07-13 14:43:21 +0800

    Get Utxos with chainx api

--- app/src/service/index.js
+++ app/src/service/index.js
@@ -7,31 +7,9 @@ export async function getBalance(addr, network = "testnet") {
   return window.fetch(url).then(res => res.json());
 }
 
-async function getTestnetUtxos(addr) {
-  const url = `https://api.chainx.org.cn/bitx/testnet/${addr}/utxos`;
-  return window.fetch(url).then(res => res.json());
-}
-
 export async function getUtxos(addr, network = "testnet") {
-  if (network === "testnet") {
-    return getTestnetUtxos(addr);
-  }
-
-  // TODO: 主网数据正在同步，暂时用blockcypher的API
-  return window
-    .fetch(
-      `https://api.blockcypher.com/v1/btc/${
-        network === "mainnet" ? "main" : "test3"
-      }/addrs/${addr}?unspentOnly=true&confirmations=1`
-    )
-    .then(response => response.json())
-    .then(data => {
-      return (data.txrefs || []).map(tx => ({
-        mintTxid: tx.tx_hash,
-        mintIndex: tx.tx_output_n,
-        value: tx.value
-      }));
-    });
+  const url = `https://api.chainx.org.cn/bitx/${network}/${addr}/utxos`;
+  return window.fetch(url).then(res => res.json());
 }
 
 export async function broadcastTx(tx, network = "testnet") {
